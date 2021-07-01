dfcc003e26d12dc21ffdbb01fb98213627518c30
Drelich-Tech <Programmer.Pete.X@gmail.com>
2021-07-01 14:05:15 +0200

    2.0

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,91 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+
+    private val _eventGameFinished = MutableLiveData<Boolean>()
+    val eventGameFinished : LiveData<Boolean>
+        get() = _eventGameFinished
+
+    // The list of words - the front of the list is the next word to guess
+    lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
+        _word.value = ""
+        _score.value = 0
+        _eventGameFinished.value = false
+    }
+    fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+            _eventGameFinished.value = true
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        _score.value = score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = score.value?.plus(1)
+        nextWord()
+    }
+
+    fun gameFinished() {
+        _eventGameFinished.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+}
